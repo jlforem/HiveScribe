add66fb37e6a631458052db8c1867e111fbbe399
jlforem <17033128@students.southwales.ac.uk>
2025-01-14 18:47:57 +0000

    SignalRIntegration folder with site put into the SignalR project razor app framework

---
+++ SignalR_Integration/HiveScribe_SignalR_Integration/Hubs/typeLink.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace HiveScribe_SignalR_Integration.Hubs
+{
+	public class typeLink : Hub
+	{
+		public async Task updateMessageBox(string message)
+		{
+			await Clients.All.SendAsync("updateMessageBox", message);
+		}
+	}
+}
